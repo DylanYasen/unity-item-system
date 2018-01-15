6b9f97c70f627d5fe9c4f4dd33015aa1ebdcaa75
Yadikaer Yasheng <yadikae1@umbc.edu>
2018-01-15 12:18:14 -0500

    added funtion to remove item in slot

--- Assets/Inventory/Scripts/InventoryBaseSlot.cs
+++ Assets/Inventory/Scripts/InventoryBaseSlot.cs
@@ -1,6 +1,7 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using uItem;
+using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace uInventory
 {
@@ -13,20 +14,35 @@ namespace uInventory
 
         public event Inventory.ItemChangedDelegate OnItemChanged = delegate { };
 
-        public InventoryBaseSlot(Inventory inventory)
+        public InventoryBaseSlot (Inventory inventory)
         {
             OwningInventory = inventory;
         }
 
-        public virtual bool SetItem(Item item, int amount = 0)
+        public virtual bool SetItem (Item item, int amount = 0)
         {
             this.Item = item;
             this.ItemAmount = amount;
 
-            OnItemChanged(item, amount);
+            OnItemChanged (item, amount);
 
             return true;
         }
 
+        public virtual int RemoveItem (int amt = 1)
+        {
+            Assert.IsTrue (ItemAmount > 0, "no items in the slot to remove");
+
+            int availableToRemove = ItemAmount;
+
+            ItemAmount -= amt;
+            if (ItemAmount < 1)
+            {
+                Item = null;
+            }
+            OnItemChanged (Item, ItemAmount);
+
+            return availableToRemove;
+        }
     }
 }
