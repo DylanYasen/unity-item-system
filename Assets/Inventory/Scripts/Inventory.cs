1e274902cefbfa7f0ec5ed0670ea8d8ecafc4a8a
Yadikaer Yasheng <yadikae1@umbc.edu>
2018-01-15 12:17:05 -0500

    added logic for finding and removing items with predicates

--- Assets/Inventory/Scripts/Inventory.cs
+++ Assets/Inventory/Scripts/Inventory.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEngine.Assertions;
 using uItem;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 namespace uInventory
 {
@@ -12,13 +13,13 @@ namespace uInventory
         public InventoryItemSlot[] ItemSlots { get; private set; }
         public int SlotCount { get; private set; }
 
-        public delegate void ItemChangedDelegate(Item item, int amount);
+        public delegate void ItemChangedDelegate (Item item, int amount);
         public event ItemChangedDelegate OnItemAdded = delegate { };
 
         private GameObject owner;
         private ItemDatabase itemDatabase;
 
-        public Inventory(GameObject inOwner, ItemDatabase inItemDatabase, int slotCount)
+        public Inventory (GameObject inOwner, ItemDatabase inItemDatabase, int slotCount)
         {
             this.owner = inOwner;
             this.SlotCount = slotCount;
@@ -28,34 +29,32 @@ namespace uInventory
             ItemSlots = new InventoryItemSlot[slotCount];
             for (int i = 0; i < slotCount; i++)
             {
-                ItemSlots[i] = new InventoryItemSlot(this);
+                ItemSlots[i] = new InventoryItemSlot (this);
             }
         }
 
-        [System.Obsolete("Use AddItem instead")]
-        public void AddItemByID(int id)
+        [System.Obsolete ("Use AddItem instead")]
+        public void AddItemByID (int id)
         {
             //@deprecated
         }
 
-        public void AddItem(string name, int amount = 1)
+        public void AddItem (string name, int amount = 1)
         {
-            Item item = itemDatabase.GetItemByName(name);
+            Item item = itemDatabase.GetItemByName (name);
             if (item != null)
             {
-                AddItem(ScriptableObject.Instantiate(item), amount);
+                AddItem (ScriptableObject.Instantiate (item), amount);
             }
         }
 
-        public void AddItem(Item item, int amount = 1)
+        public void AddItem (Item item, int amount = 1)
         {
-            bool stacked = StackItem(item, amount);
+            bool stacked = StackItem (item, amount);
             if (!stacked)
             {
-                bool added = AddNewItem(item, amount);
-                if (added)
-                {
-                }
+                bool added = AddNewItem (item, amount);
+                if (added) { }
                 else
                 {
                     // @todo: notify failed to add
@@ -63,15 +62,63 @@ namespace uInventory
             }
         }
 
-        public void RemoveAllItems()
+        public bool RemoveItems (Item item, int amt = 1)
+        {
+            if (!HasItem (item, amt))
+            {
+                Debug.LogErrorFormat ("don't have %d $s to remove", amt, item.name);
+                return false;
+            }
+
+            InventoryBaseSlot[] slots = Array.FindAll (ItemSlots, slot => slot.ContainsItem && slot.Item.name == item.name);
+
+            int amtRemainingToRemove = amt;
+            foreach (var slot in slots)
+            {
+                int removedAmt = slot.RemoveItem (amtRemainingToRemove);
+                amtRemainingToRemove -= removedAmt;
+                if (amtRemainingToRemove == 0)
+                {
+                    break;
+                }
+            }
+            return true;
+        }
+
+        public void RemoveAllItems ()
         {
             foreach (var slot in ItemSlots)
             {
-                slot.SetItem(null);
+                slot.SetItem (null);
             }
         }
 
-        private bool StackItem(Item newItem, int amount)
+        public Item[] FindAllItems (System.Predicate<Item> predicate)
+        {
+            InventoryBaseSlot[] slots = Array.FindAll (ItemSlots, slot => slot.ContainsItem && predicate (slot.Item));
+
+            // @todo: optimize allocation
+            Item[] items = new Item[slots.Length];
+            for (int i = 0; i < slots.Length; i++)
+            {
+                items[i] = slots[i].Item;
+            }
+            return items;
+        }
+
+        public bool HasItem (Item item, int amt = 1)
+        {
+            InventoryBaseSlot[] slots = Array.FindAll (ItemSlots, slot => slot.ContainsItem && slot.Item.name == item.name);
+            int availableAmt = 0;
+            foreach (var slot in slots)
+            {
+                availableAmt += slot.ItemAmount;
+            }
+
+            return availableAmt >= amt;
+        }
+
+        private bool StackItem (Item newItem, int amount)
         {
             if (!newItem.IsStackable)
             {
@@ -86,7 +133,7 @@ namespace uInventory
                     if (slot.Item.name == newItem.name)
                     {
                         // @todo: stack items
-                        OnItemAdded(newItem, amount);
+                        OnItemAdded (newItem, amount);
                         return true;
                     }
                 }
@@ -95,7 +142,7 @@ namespace uInventory
             return false;
         }
 
-        private bool AddNewItem(Item newItem, int amount)
+        private bool AddNewItem (Item newItem, int amount)
         {
             for (int i = 0; i < ItemSlots.Length; i++)
             {
@@ -103,11 +150,11 @@ namespace uInventory
 
                 if (!slot.ContainsItem)
                 {
-                    slot.SetItem(newItem, amount);
+                    slot.SetItem (newItem, amount);
 
                     // @todo: auto equip
 
-                    OnItemAdded(newItem, amount);
+                    OnItemAdded (newItem, amount);
                     return true;
                 }
             }
