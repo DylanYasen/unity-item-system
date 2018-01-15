4efef48029f6fbd804fb1d93d38126e39642c8bb
Yadikaer Yasheng <yadikae1@umbc.edu>
2018-01-15 12:25:59 -0500

    updated prefab

--- Assets/Inventory/Scripts/InventoryBaseSlotUI.cs
+++ Assets/Inventory/Scripts/InventoryBaseSlotUI.cs
@@ -1,15 +1,15 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
+﻿using uItem;
+using UnityEngine;
 using UnityEngine.Assertions;
-using uItem;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace uInventory
 {
-    [RequireComponent(typeof(Image))]
+    [RequireComponent (typeof (Image))]
     public abstract class InventoryBaseSlotUI : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler, IDragHandler
     {
-        public delegate void SlotInteractionDelegate(InventoryBaseSlot slot);
+        public delegate void SlotInteractionDelegate (InventoryBaseSlot slot);
         public event SlotInteractionDelegate OnLeftClickSlot = delegate { };
         public event SlotInteractionDelegate OnRightClickSlot = delegate { };
         public event SlotInteractionDelegate OnMouseEnterSlot = delegate { };
@@ -21,29 +21,32 @@ namespace uInventory
         public Image BackgroundImage;
         public Image ItemImage;
 
-        protected virtual void Awake()
+        protected virtual void Awake ()
         {
-            Assert.IsNotNull(BackgroundImage, "Inventory slot doesn't have a image component");
-            Assert.IsNotNull(ItemImage, "Inventory slot doesn't have a child component called 'ItemImage' with image component");
+            Assert.IsNotNull (BackgroundImage, "Inventory slot doesn't have a image component");
+            Assert.IsNotNull (ItemImage, "Inventory slot doesn't have a child component called 'ItemImage' with image component");
 
             ItemImage.enabled = false;
         }
 
-        protected virtual void OnEnable()
+        protected virtual void OnEnable ()
         {
             // @todo: better to set before becoming visiable
-            UpdateUI(slot.Item, slot.ItemAmount);
+            if (slot != null)
+            {
+                UpdateUI (slot.Item, slot.ItemAmount);
+            }
         }
 
-        public virtual void SetSlot(InventoryBaseSlot slot)
+        public virtual void SetSlot (InventoryBaseSlot slot)
         {
-            Assert.IsNotNull(slot, "null inventory slot model is passed to inventory slot ui");
+            Assert.IsNotNull (slot, "null inventory slot model is passed to inventory slot ui");
 
             this.slot = slot;
             this.slot.OnItemChanged += UpdateUI;
         }
 
-        protected virtual void UpdateUI(Item item, int amount)
+        protected virtual void UpdateUI (Item item, int amount)
         {
             if (item != null)
             {
@@ -57,34 +60,34 @@ namespace uInventory
             }
         }
 
-        public void OnPointerDown(PointerEventData eventData)
+        public void OnPointerDown (PointerEventData eventData)
         {
             if (slot == null) { return; }
 
             PointerEventData.InputButton button = eventData.button;
             if (button == PointerEventData.InputButton.Left)
             {
-                OnLeftClickSlot(slot);
+                OnLeftClickSlot (slot);
             }
             else if (button == PointerEventData.InputButton.Right)
             {
-                OnRightClickSlot(slot);
+                OnRightClickSlot (slot);
             }
         }
 
-        public virtual void OnPointerEnter(PointerEventData eventData)
+        public virtual void OnPointerEnter (PointerEventData eventData)
         {
-            OnMouseEnterSlot(slot);
+            OnMouseEnterSlot (slot);
         }
 
-        public virtual void OnPointerExit(PointerEventData eventData)
+        public virtual void OnPointerExit (PointerEventData eventData)
         {
-            OnMouseExitSlot(slot);
+            OnMouseExitSlot (slot);
         }
 
-        public virtual void OnDrag(PointerEventData eventData)
+        public virtual void OnDrag (PointerEventData eventData)
         {
-            OnMouseDragSlot(slot);
+            OnMouseDragSlot (slot);
         }
     }
 }
