ed3998f9309c95527aa2a3c6e9cd0b920387273b
Yadikaer Yasheng <yadikae1@umbc.edu>
2018-06-03 11:27:58 -0400

    setup demo scene

---
+++ Assets/demo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using uInventory;
+using uItem;
+using UnityEngine;
+
+public class demo : MonoBehaviour
+{
+	public InventoryUIManager inventoryUIManager;
+	public InventoryUI inventoryUI;
+	public ItemDatabase itemDatabase;
+
+	public Inventory inventory;
+
+	void Start ()
+	{
+		inventory = new Inventory (gameObject, itemDatabase, 12);
+		inventoryUI.SetInventory (inventory, inventoryUIManager);
+	}
+}
